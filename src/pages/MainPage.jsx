e2248e5c804567941ff6af39ffcae5dfeddbe508
Olesya Figurovskaya <lisonkadrug@gmail.com>
2026-08-17 09:43:42 +0300

    test fetch

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,34 @@
+import { useState } from 'react'
 function MainPage() {
+    const [post, setPost] = useState(null);
+    const fetchDate = async () => {
+        try {
+            const res = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+            if (!res.ok) {
+                throw new Error(`net: ${res.status}`)
+            }
+            const result = await res.json();
+            setPost(result)
+        } catch (error) {
+            console.log(error.message)
+        } finally {
+            console.log('all')
+        }
+
+    }
+
+    fetchDate();
+
+
     return <div>
         <div className="mainPage">
             <h1>Это MainPage</h1>
+            {post && (
+                <article>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                </article>
+            )}
         </div>
     </div>
 }
